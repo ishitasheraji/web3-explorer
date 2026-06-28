a3c3d7b547ebc81da9e4512920d6de6c916f7836
ishitasheraji <ishitasheraji@gmail.com>
2026-06-28 11:00:28 +0530

    Fix GitHub Pages base path for router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web3-explorer">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
